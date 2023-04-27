acbb898343cc47af814c0f56c5ed2c7b27c44fab
mqrtln <mart0l@outlook.com>
2023-04-28 01:04:44 +0200

    Adding  some websockets to server.js

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import path from "path";
+import WebSocket, { WebSocketServer } from 'ws';
 
 const app = express();
 app.use(express.static("../client/dist"));
@@ -11,6 +12,33 @@ app.use((req, res, next) => {
   }
 });
 
+
+const wsServer = new WebSocketServer({ noServer: true });
+
+const sockets = []; 
+
+
+wsServer.on("connect", (socket) => {
+  sockets.push(socket);
+  console.log("ws connected");
+  socket.send(
+    JSON.stringify({ author: "SERVER", message: "Type anything you want!" })
+  );
+
+  socket.on("message", (data) => {
+    const { author, message } = JSON.parse(data);
+    for (const recipient of sockets) {
+      recipient.send(JSON.stringify({ author, message }));
+    }
+  });
+});
+
+
 const server = app.listen(5000, () => {
   console.log(`Started on http://localhost:${server.address().port}`);
+  server.on("upgrade", (request, socket, head) => {
+    wsServer.handleUpgrade(request, socket, head, (socket) => {
+      wsServer.emit("connect", socket, request);
+    });
+  });
 });
