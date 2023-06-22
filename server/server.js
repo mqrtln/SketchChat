273f4373a1c2ae36861e6a6184be47b2e7be948f
kajahaugen <69812514+kajahaugen@users.noreply.github.com>
2023-06-22 14:05:23 +0200

    changed to localhost 5001 for kajamac

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import path from "path";
-import { WebSocketServer } from 'ws';
+
 
 const app = express();
 app.use(express.static("../client/dist"));
@@ -13,41 +13,13 @@ app.use((req, res, next) => {
 });
 
 
-const wsServer = new WebSocketServer({ noServer: true });
 
 const sockets = []; 
 
 
-wsServer.on("connect", (socket) => {
-  sockets.push(socket);
-  console.log("ws connected");
-  socket.send(
-    JSON.stringify({ 
-      displayName: 'SERVER',
-      message: 'Type anything you want!',
-      color: '#000000',
-    })
-  );
-
-  socket.on("message", (data) => {
-    const {message, displayName, color } = JSON.parse(data);
-    const messageData = { message, displayName, color };
-    for (const recipient of sockets) {
-      recipient.send(JSON.stringify(messageData));
-    }
-  })
 
 
-  socket.on("close", () => {
-    sockets.splice(sockets.indexOf(socket), 1);
-    console.log("ws disconnected");
-  });
-  
-
-});
-
-
-const server = app.listen(5000, () => {
+const server = app.listen(5001, () => {
   console.log(`Started on http://localhost:${server.address().port}`);
   server.on("upgrade", (request, socket, head) => {
     wsServer.handleUpgrade(request, socket, head, (socket) => {
